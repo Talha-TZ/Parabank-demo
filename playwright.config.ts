3b7047a830dd9fbb49cecc508ff840d2e2b9c036
Talha Siddeeqi <talha.techzango@gmail.com>
2026-07-17 15:22:48 +0500

    Initial commit

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,6 @@
+
 import { defineConfig, devices } from '@playwright/test';
+
 export default defineConfig({
 timeout: 30 * 1000, //30000 ms(30 secs)
 testDir: './tests',
@@ -15,7 +17,7 @@ use: {
 trace: 'on-first-retry',
 screenshot: 'only-on-failure',
 video: 'retain-on-failure',
-headless: false,
+headless: true,
 viewport: { width: 1280, height: 720 }, // Set default viewport size for consistency
 ignoreHTTPSErrors: true, // Ignore SSL errors if necessary
 permissions: ['geolocation'], // Set necessary permissions for geolocation-based tests
